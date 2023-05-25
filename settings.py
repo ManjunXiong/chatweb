7d7539c50ccc14a7bb13f4cef8d1f0c31fc895ef
Manjun Xiong <93245166+ManjunXiong@users.noreply.github.com>
2023-05-25 16:05:16 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 # SECRET_KEY（flask项目密钥,不用管,也用不到）
-SECRET_KEY = "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"
+SECRET_KEY = "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"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "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"
 
 # openAi 官方 api
 # URL = "https://api.openai.com/v1/chat/completions"
