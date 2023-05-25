d2e2df95a55f3c0bb1fd8c4c7f502545272c3cc8
Manjun Xiong <93245166+ManjunXiong@users.noreply.github.com>
2023-05-25 15:51:26 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# SECRET_KEY（flask项目密钥,不用管,也用不到）
+SECRET_KEY = "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"
+
+# openAi api key
+OPENAI_API_KEY = "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"
+
+# openAi 官方 api
+# URL = "https://api.openai.com/v1/chat/completions"
+
+# openAi 代理 api
+URL = "https://open.aiproxy.xyz/v1/chat/completions"
